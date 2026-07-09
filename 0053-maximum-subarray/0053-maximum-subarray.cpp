5c792439d6bc12c96b51b444e1705342b03058dc
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-07-09 22:08:48 +0530

    Time: 0 ms (100%), Space: 71.8 MB (14.51%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int i=0; int n=nums.size();
+        int best_ending=nums[0];
+        int ans=nums[0];
+
+        for(int i=1;i<n;i++){
+            int v1=best_ending+nums[i];
+            int v2=nums[i];
+            best_ending=max(v1,v2);
+            ans=max(ans,best_ending);
+        }
+        //if(nums[0]==1){ return 1;}
+        //if(nums[0]==0){ return 0;}
+        return ans;
+    }
+};
