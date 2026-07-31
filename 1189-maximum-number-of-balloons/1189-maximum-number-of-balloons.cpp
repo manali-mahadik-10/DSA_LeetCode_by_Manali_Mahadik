858447bea5131ee7fea7ef48b85624fce6f0ca25
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-07-31 23:50:23 +0530

    Time: 3 ms (11.82%), Space: 9.1 MB (6.87%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int b=0, a=0, l=0, o=0, n=0;
-        for(int i=0;i<text.size();i++){
-            if(text[i]=='b') b++;
-            if(text[i]=='a')a++;
-            if(text[i]=='l')l++;
-            if(text[i]=='o')o++;
-            if(text[i]=='n')n++;
+        string s; int n=text.size();
+        unordered_map<char, int> have;
+        for(int i=0;i<n;i++){
+            have[text[i]]++;
         }
-        int T_b=b/1;
-        int T_a=a/1;
-        int T_l=l/2;
-        int T_o=o/2;
-        int T_n=n/1;
-        return min({T_b,T_a,T_l,T_o,T_n});
-
+        unordered_map<char, int> need;
+        need['b']=1;
+        need['a']=1;
+        need['l']=2;
+        need['o']=2;
+        need['n']=1;
+        int res=INT_MAX;
+        for(auto i: need){
+            char c=i.first;
+            int fneed=i.second;
+            int fhave=have[c];
+            int times=fhave/fneed;
+            res=min(res, times);
+        }
+        return res;
     }
 };
