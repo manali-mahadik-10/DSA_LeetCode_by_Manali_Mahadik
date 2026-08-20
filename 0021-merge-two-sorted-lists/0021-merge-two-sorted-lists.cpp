c03dc23949b449c32056837e1ae57032966a58ce
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-08-20 21:52:20 +0530

    Time: 0 ms (100%), Space: 19.4 MB (88.24%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,38 +11,28 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode();
-        ListNode* tail = dummy;
-
-        while(list1 != nullptr && list2 != nullptr){
-            if(list1->val <= list2->val)
-            {
-                tail->next = list1;
-                list1 = list1->next;
+        ListNode *dummy=new ListNode(-1);
+        ListNode *tail=dummy;
+        if(list1==nullptr && list2==nullptr) return nullptr;
+        if(list1==nullptr) return list2;
+        if(list2==nullptr) return list1;
+        while(list1!=nullptr && list2!=nullptr){
+            if(list1->val<=list2->val){
+                tail->next=list1;
+                list1=list1->next;
             }
-            else
-            {
-                tail->next = list2;
-                list2 = list2->next;
+            else{
+                tail->next=list2;
+                list2=list2->next;
             }
-
-            tail = tail->next;
+            tail=tail->next;
+            if(list1==nullptr){
+                tail->next=list2;
+            }
+            else{
+                tail->next=list1;
+            }
         }
-
-        while(list1 != nullptr){
-            tail->next = list1;
-            list1 = list1->next;
-            tail = tail->next;
-        }
-
-        while(list2 != nullptr){
-            tail->next = list2;
-            list2 = list2->next;
-            tail = tail->next;
-        }
-
-        ListNode* newHead = dummy->next;
-        delete dummy;
-        return newHead;
+        return dummy->next;
     }
 };
