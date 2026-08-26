e877509eb8ab612099910596a5430d27e74aab4a
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-08-26 15:51:09 +0530

    Time: 4 ms (55.55%), Space: 22.1 MB (55.28%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-//go from left and right if at left and right value is not equal once slip left and right  and if any of them is palindrom then ok otherwise false;
-    bool isPalindrome (string sub)
-    {
-        int l=0,r=sub.size()-1;
-        while(l<r)
-        {
-            if(sub[l]!=sub[r])  return false;
+    bool ispalindrome(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
             l++;
             r--;
         }
         return true;
     }
+
     bool validPalindrome(string s) {
-        int n=s.size();
-        int left=0,right=n-1;
-        while(left<right)
-        {
-            if(s[left]==s[right])
-            {
-                left++;
-                right--;
-            }
-            else
-            {
-                return isPalindrome(s.substr(left+1,right-left))|| isPalindrome(s.substr(left,right-left));
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l] == s[r]) {
+                l++;
+                r--;
+            } else {
+                return ispalindrome(s, l, r - 1) || ispalindrome(s, l + 1, r);
             }
         }
         return true;
