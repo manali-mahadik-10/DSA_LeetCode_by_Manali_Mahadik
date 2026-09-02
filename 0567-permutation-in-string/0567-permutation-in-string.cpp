69f1ea8f4c1265bc037c943622ad73196cc0e24a
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-09-02 23:19:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n=s1.length();
-        int m=s2.length();
-        vector<int>f1(26,0);
-        vector<int>f2(26,0);
-        for(int i=0;i<n;i++){
-            f1[s1[i]-'a']++;
+        int n1=s1.size();
+        int n2=s2.size();
+        if(n1>n2) return false;
+        vector<int>arr1(26,0);
+        vector<int>arr2(26,0);
+        for(int i=0;i<s1.size();i++){
+            arr1[s1[i]-'a']++;
+            arr2[s2[i]-'a']++;
         }
-        int i=0;
-        for(int j=0;j<m;j++){
-            f2[s2[j]-'a']++;
-            if(j-i+1 > n){
-                f2[s2[i]-'a']--;
-                i++;
-            }
-            if(j-i+1==n){
-                int f=0;
-                for(int k=0;k<26;k++){
-                    if(f1[k]!=f2[k]){
-                        f=1;
-                        break;
-                    }
-                }
-                if(f!=1){
-                    return true;
-                }
-            }
+        if(arr1==arr2) return true;
+        int left=0;
+        for(int i=n1;i<n2;i++){
+            arr2[s2[i]-'a']++;
+            arr2[s2[left]-'a']--;
+            left++;
+            if(arr1==arr2) return true;
         }
         return false;
     }
