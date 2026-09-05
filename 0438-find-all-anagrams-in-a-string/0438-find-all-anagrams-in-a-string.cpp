207e09fc1cc8cda390aaa898c86c11ba634f6a91
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-09-05 21:32:37 +0530

    Time: 1012 ms (5%), Space: 424.1 MB (5%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,41 @@
 class Solution {
-public:
+   bool isAnagram(string s, string p) {
+   
+    if (s.length() != p.length()) {
+        return false;
+    }
+
+    vector<int> count1(26, 0);
+    vector<int> count2(26, 0);
+
+    for (int i = 0; i < s.length(); i++) {
+        count1[s[i] - 'a']++;
+        count2[p[i] - 'a']++;
+    }
+
+    if (count1 == count2) {
+        return true;
+    }
+    
+    return false; 
+}
+    
+    public :
     vector<int> findAnagrams(string s, string p) {
-        int s_len = s.length();
-        int p_len = p.length();
-        
-        if(s.size() < p.size()) return {};
-        
-        vector<int> freq_p(26,0);
-        vector<int> window(26,0);
-        
-        //first window
-        for(int i=0;i<p_len;i++){
-            freq_p[p[i]-'a']++;
-            window[s[i]-'a']++;
-        }
-        
-        vector<int> ans;
-        if(freq_p == window) ans.push_back(0);
-        
-        for(int i=p_len;i<s_len;i++){
-            window[s[i-p_len] - 'a']--;
-            window[s[i] - 'a']++;
-            
-            if(freq_p == window) ans.push_back(i-p_len+1);
-        }
-        return ans;
+        vector<int>result ;
+      int k = p.length();
+      int left = 0;
+    if( k > s.length() )return result;
+      for( int right = 0 ; right < s.length() ; right++){
+           if( right - left + 1 == k){
+
+           string currentwindow = s.substr( left , k);
+           if( isAnagram( currentwindow , p)){
+                   result.push_back(left);
+           }
+           left++;
+           }
+      }
+      return result ;
     }
 };
