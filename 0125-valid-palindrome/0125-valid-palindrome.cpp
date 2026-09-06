fd200c6b62e274dd8d4113d953dc7b32b6544a6e
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-09-06 22:29:41 +0530

    Time: 0 ms (100%), Space: 10.1 MB (28.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(string s) {
        int start=0;
        int end=s.size()-1;
-       while(start<=end){
+       while(start<end){
            if(!isalnum(s[start])){start++; continue;}
            if(!isalnum(s[end])){end--;continue;}
            if(tolower(s[start])!=tolower(s[end]))return false;
