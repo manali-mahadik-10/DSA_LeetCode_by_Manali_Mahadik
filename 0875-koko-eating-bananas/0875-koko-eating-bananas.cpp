ea9101d409ed1d6179d59038b42c287cee506603
manali-mahadik-10 <manalimahadik14@gmail.com>
2026-09-18 17:58:33 +0530

    Time: 9 ms (43.99%), Space: 23 MB (24.39%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1, r = *max_element(piles.begin(), piles.end());
-        int ans = r;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            long hours = 0;
-            for (int p : piles)
-                hours += (p + mid - 1) / mid;
-
-            if (hours <= h) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+    long fun(vector<int>&piles, int n, int speed){
+        long h=0;
+        for(int i=0;i<n;i++){
+            h=h+piles[i]/speed;
+            if(piles[i]%speed!=0){
+                h++;
             }
         }
-
-        return ans;
+        return h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size(); int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(piles[i]>maxi){
+                maxi=piles[i];
+            }
+        }
+        int low=1, high=maxi;
+        int res=-1;
+        while(low<=high){
+            int guess=(low+high)/2;
+            long hour=fun(piles,n,guess);
+            if(hour>h){
+                low=guess+1;
+            }
+            else{
+                res=guess;
+                high=guess-1;
+            }
+        }
+        return res;
     }
 };
